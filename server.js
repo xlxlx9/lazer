0d7cdbe15f2b1fc53f0436e1bd880032f41b548d
Lei <xulei@gatech.edu>
2016-03-06 16:37:07 -0500

    [*] more friendly output.

--- server.js
+++ server.js
@@ -43,13 +43,13 @@ var router = express.Router();              // get an instance of the express Ro
 // middleware to use for all requests
 router.use(function(req, res, next) {
     // do logging
-    console.log('Something is happening.');
+    console.log("Someone is up to someting! ");
     next(); // make sure we go to the next routes and don't stop here
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ msg: "We like you!" });
 });
 
 // more routes for our API will happen here
