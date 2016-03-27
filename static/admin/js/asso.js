50ff1540c7b8fba465a50ab0c3bc34c785367d54
Lei <xulei@gatech.edu>
2016-03-27 15:22:37 -0400

    [+] detail page works with data bound to factory obj.

--- static/admin/js/asso.js
+++ static/admin/js/asso.js
@@ -1,16 +1,19 @@
 Asso = function() {
-	function assignChannelsToSources(scope) {
+	function assignChannelsToSources(scope, R0) {
 		if(null == scope.sources) return;
 		if(null == scope.channels) return;
 		
-		var id2src = scope.srcmap = {};
+		var id2src = {};
+		R0.id2src = id2src;
 		for(var i = 0; i < scope.sources.length; i++) {
 			var src_i = scope.sources[i];
 			id2src[src_i._id] = src_i;
 			src_i.tags = [];
 		}
+		R0.id2ch = {};
 		for(var i = 0; i < scope.channels.length; i++) {
 			var ch_i = scope.channels[i];
+			R0.id2ch[ch_i._id] = ch;
 			for(var j = 0; j < ch_i.sources.length; j++) {
 				var src_id = ch_i.sources[j];
 				//console.log("Adding tag " + ch_i.title + " to " + src_id);
