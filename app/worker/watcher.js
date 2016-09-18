3d4105e66b0fd222bcd69c2c0e1739742afb0f01
Lei <xulei@gatech.edu>
2016-09-17 23:59:13 -0400

    [+] Remove old feeds too.

--- app/worker/watcher.js
+++ app/worker/watcher.js
@@ -3,7 +3,7 @@ var Watcher = function() {
 
 Watcher.prototype.cleanup = function(days) {
 	var cut_date = new Date()
-	cut_date.setDate(-days)
+	cut_date.setDate(cut_date.getDate() - days)
 	console.log("cut date " + cut_date)
 	var Item = require("../models/item")
 	Item.remove({ "date": {"$lt": cut_date } }, function(err, any) {
@@ -11,14 +11,16 @@ Watcher.prototype.cleanup = function(days) {
 			console.log("Failed to remove old items")
 			return
 		}
-		console.log("result: " + any)
+		console.log("Remove item result: " + any)
 	})
-	return
-	Item.remove({ "date": {"$lt": cut_date} })
-	console.log("Items older that %d days removed. ", days)
 	var Feed = require("../models/feed")
-	Feed.remove({ "since": {"$lt": cut_date} })
-	console.log("Feeds older that %d days removed. ", days)
+	Feed.remove({ "since": {"$lt": cut_date} }, function(err, any) {
+		if(err) {
+			console.log("Failed to remove old feeds")
+			return
+		}
+		console.log("Remove feed result: " + any)
+	})
 }
 
 module.exports = new Watcher()
